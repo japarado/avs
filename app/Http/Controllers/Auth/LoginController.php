f2f1687ef8dcf341192c905d1e1cfdcf1c4e8c31
Justin Parado <justin.parado@dice205.com>
2020-04-03 13:04:49 +0800

    Login prompt only accessible when logging in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/prompt";
 
     /**
      * Create a new controller instance.
@@ -35,7 +35,8 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+		$this->middleware('guest')->except(['logout', 'prompt']);
+		$this->middleware('auth')->only('prompt');
     }
 
     public function prompt()
