e360a97543e4e8a0c7e94d7c25d403e4e287ec26
Nicole Lopez <nicoleeduworklopez@gmail.com>
2020-04-04 07:19:05 +0800

    registry view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,7 +14,7 @@
                             <a href="{{route('admin.candidates')}}" class="btn btn-lg client-custom-button admin-dashboard__button">edit candidates</a>
                             </div>
                             <div class="admin-dashboard__button-container">
-                                <a class="btn btn-lg client-custom-button-2 admin-dashboard__button">student registry</a>
+                                <a href="{{route('admin.registry')}}" class="btn btn-lg client-custom-button-2 admin-dashboard__button">student registry</a>
                             </div>
                             <div class="admin-dashboard__button-container">
                                 <a class="btn btn-lg client-custom-button admin-dashboard__button">results</a>
---
+++ resources/views/admin/registry.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/app')
+@section('header')
+@include('inc/dashboard-header')
+@endsection
+@section('content')
+<article class="admin-dashboard">
+    <div class="admin-dashboard__header mt-4">student registry</div>
+    <div class="admin-dashboard__container">
+        <div class="admin-dashboard__actions">
+            <div class="admin-dashboard__card">
+                <div class="admin-dashboard__card-body">
+                    <div class="admin-dashboard__card-body-container">
+                        <div class="d-flex flex-column">
+                            <div class="admin-dashboard__button-container">
+                                <a href="{{route('admin.registrySection')}}" class="btn btn-lg client-custom-button admin-dashboard__button">sections</a>
+                            </div>
+                            <div class="admin-dashboard__button-container">
+                                <a href="{{route('admin.registryStudent')}}" class="btn btn-lg client-custom-button-2 admin-dashboard__button">students</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</article>
+@endsection
+
+@section('footer')
+    @include('inc.admin-footer')
+@endsection
