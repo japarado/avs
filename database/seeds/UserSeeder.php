56ace626205fd9cbe65d107de1578dc9675ebd5c
Justin Parado <justin.parado@dice205.com>
2020-04-03 13:18:12 +0800

    Added seeded student

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -6,19 +6,27 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
 		/* Create super user */
 		DB::table('user')->insert([
-			'name' => 'Admin',
-			'email' => 'admin@admin.com',
-			'password' => Hash::make('password123'),
-			'role_id' => config('constants.roles.admin')
+			[
+				'name' => 'Admin',
+				'email' => 'admin@admin.com',
+				'password' => Hash::make('password123'),
+				'role_id' => config('constants.roles.admin')
+			],
+			[
+				'name' => 'Student',
+				'email' => '123456',
+				'password' => Hash::make('password123'),
+				'role_id' => config('constants.roles.student')
+			]
 		]);
-    }
+	}
 }
