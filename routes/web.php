b91bc272bcb1faf463973a8b030d15e588821407
Justin Parado <justin.parado@dice205.com>
2020-04-03 09:07:21 +0800

    Revise route

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('auth/login');
-});
-
 Auth::routes();
+
+Route::get("/", function() {
+	return redirect()->route('login');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
 
