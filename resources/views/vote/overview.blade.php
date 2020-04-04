1425a958b300bd477f54fb453387774c2926dc9c
Justin Parado <justin.parado@dice205.com>
2020-04-04 11:34:06 +0800

    Integrated vote overview page

--- resources/views/vote/overview.blade.php
+++ resources/views/vote/overview.blade.php
@@ -10,64 +10,25 @@
 					<div class="overview__header"><span>Overview</span></div>
 					<div class="overview__body">
 						<div class="overview__body-container">
-							@foreach($candidates as $candidate)
-								@if($candidate['type'] == 'president')
-									<div class="overview__entry">
-										<div class="overview__title"><span>President</span></div>
+							@foreach($positions as $position)
+								<div class="overview__entry">
+									<div class="overview__title"><span>{{ $position->name }}</span></div>
+									@foreach($position->candidates as $candidate)
 										<div class="overview__value">
-											<input type="hidden" value="1" />
+											<input type="hidden" value="" />
 											<textarea class="overview__input form-control"
-													  name="president">{{$candidate['name']}}</textarea>
+													  name="president">
+												{{$candidate->type === config('constants.candidatetypes.regular') ? $candidate->name : ABSTAIN }}
+											</textarea>
 										</div>
-									</div>
-								@endif
-								@if($candidate['type'] == 'vice_president')
-									<div class="overview__entry">
-										<div class="overview__title"><span>Vice President</span></div>
-										<div class="overview__value">
-											<input type="hidden" value="1" />
-											<textarea class="overview__input form-control"
-													  name="vice_president">{{$candidate['name']}}</textarea>
-										</div>
-									</div>
-								@endif
-								@if($candidate['type'] == 'secretary')
-									<div class="overview__entry">
-										<div class="overview__title"><span>Secretary</span></div>
-										<div class="overview__value">
-											<input type="hidden" value="1" />
-											<textarea class="overview__input form-control"
-													  name="secretary">{{$candidate['name']}}</textarea>
-										</div>
-									</div>
-								@endif
-								@if($candidate['type'] == 'treasurer')
-									<div class="overview__entry">
-										<div class="overview__title"><span>Treasurer</span></div>
-										<div class="overview__value">
-											<input type="hidden" value="1" />
-											<textarea class="overview__input form-control"
-													  name="treasurer">{{$candidate['name']}}</textarea>
-										</div>
-									</div>
-								@endif
-
-								@if($candidate['type'] == 'auditor')
-									<div class="overview__entry">
-										<div class="overview__title"><span>Auditor</span></div>
-										<div class="overview__value">
-											<input type="hidden" value="1" />
-											<textarea class="overview__input form-control"
-													  name="auditor">{{$candidate['name']}}</textarea>
-										</div>
-									</div>
-								@endif
+									@endforeach
+								</div>
 							@endforeach
 						</div>
 					</div>
 				</div>
 				<div class="d-flex justify-content-center">
-					<a href="{{route('dashboard.vote',['showVotePrompt'=>true])}}" class="btn btn-lg client-custom-button-2">I
+					<a href="{{ action('VoteController@restart') }}" class="btn btn-lg client-custom-button-2">I
 						change my
 						mind</a>
 					<button type="submit" class="btn btn-lg client-custom-button ml-4">Vote</button>
