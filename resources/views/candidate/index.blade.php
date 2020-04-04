eec7efe8d9959fa28482976cd8c360dbe0ef9648
Justin Parado <justin.parado@dice205.com>
2020-04-04 17:36:17 +0800

    Replace candidate index with nicole's

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -1,20 +1,95 @@
-<a href="{{ action('CandidateController@create') }}">Create</a>
-<table>
-	@foreach($candidates as $candidate)
-		<tr>
-			<td>{{ $candidate->name }}</td>
-			<td>{{ $candidate->strand->name }}</td>
-			<td>{{ $candidate->position->name }}</td>
-			<td>
-				<a href="{{ action('CandidateController@edit', ['id' => $candidate->id]) }}">Edit</a>
-			</td>
-			<td>
-				<form action="{{ action('CandidateController@destroy', ['id' => $candidate->id]) }}" method="post">
-					@csrf 
-					@method('DELETE')
-					<button type="submit">Delete</button>
-				</form>
-			</td>
-		</tr>
-	@endforeach
-</table>
+@extends('layouts/app')
+@section('header')
+@include('inc/dashboard-header')
+@endsection
+@section('content')
+<article class="candidates">
+    <div class="candidates__container">
+        <div class="candidates__header">
+            <div class="candidates__header-container">Candidates</div>
+        </div>
+        <div class="candidates__body">
+            <div class="candidates__body-container">
+                {{-- CUT --}}
+                @foreach ($positions as $position)
+                <div class="candidates__position">
+                    <div class="candidates__position-container">
+                    <div class="candidates__position-header">{{ $position->name }}</div>
+                        <div class="candidates__position-body">
+                            <div class="candidates__position-body-container">
+                                <div class="candidates__table-container">
+                                    <table class="candidates__table">
+                                        <thead>
+                                            <tr>
+                                                <th>name</th>
+                                                <th>strand</th>
+                                                <th>position</th>
+                                                <th>image</th>
+                                                <th>actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @if(count($position['candidates']) > 0)
+												@foreach ($position->candidates as $candidate)
+                                                    <tr class="{{$candidate['isHidden'] ?? false ? "candidates__tr-border":""}}">
+                                                        <td>{{$candidate['name']}}</td>
+                                                        <td>{{$candidate['strand'][0]['name']}}</td>
+                                                        <td>{{$position['name']}}</td>
+                                                        <td><img class="candidates__img" src="{{$candidate['image']}}" alt="candidate image"/></td>
+                                                        <td>
+                                                            <div class="candidates__button-container">
+                                                                <a href="{{route('admin.candidatesUpdate')}}" class="btn candidates__table-button client-custom-button">update</a>
+                                                            </div>
+                                                            @if($candidate['isHidden'] ?? false)
+                                                            <form class="candidates__button-container">
+                                                                <button class="btn candidates__table-button client-custom-button-2">show</button>
+                                                            </form>
+                                                            @else
+                                                            <form class="candidates__button-container">
+                                                                <button class="btn candidates__table-button client-custom-button-2">hide</button>
+                                                            </form>
+                                                            @endif
+                                                            <form class="candidates__button-container">
+                                                                <button class="btn candidates__table-button client-custom-button">remove</button>
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @else
+                                                <tr>
+                                                    <td>[empty]</td>
+                                                    <td>[empty]</td>
+                                                    <td>[empty]</td>
+                                                    <td>[empty]</td>
+                                                    <td></td>
+                                                </tr>
+                                            @endif
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td>
+                                                    <a href="{{route('admin.candidatesAdd')}}" class="btn candidates__table-button client-custom-button">add</a>
+                                                </td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="d-flex justify-content-center mb-4">
+            <a href="{{route('admin.dashboard')}}" class="btn btn-lg client-custom-button">Home</a>
+        </div>
+    </div>
+</article>
+@endsection
+
+@section('footer')
+    @include('inc.admin-footer')
+@endsection
