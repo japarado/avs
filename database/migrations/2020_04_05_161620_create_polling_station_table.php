5588f6e69870384f862ca32e70cf4250c3fccbbe
Justin Parado <justin.parado@dice205.com>
2020-04-06 01:11:55 +0800

    polling station table

---
+++ database/migrations/2020_04_05_161620_create_polling_station_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePollingStationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polling_station', function (Blueprint $table) {
+            $table->id();
+			$table->string('name')->nullable()->default('RM. XXXA');
+			$table->string('auth_key');
+			$table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polling_station');
+    }
+}
