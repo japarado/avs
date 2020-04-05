e0fdca43c94e8f0ff2d4a18919df479294ba50b0
Justin Parado <justin.parado@dice205.com>
2020-04-06 01:12:40 +0800

    formatting

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,31 +7,31 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		/* Create super user */
-		DB::table('user')->insert([
-			[
-				'name' => 'Admin',
-				'email' => 'admin@admin.com',
-				'password' => Hash::make('password123'),
-				'role_id' => config('constants.roles.admin'),
-				'section_id' => null,
-				'class_number' => null,
-			],
-			[
-				'name' => 'Student',
-				'email' => '123456',
-				'password' => Hash::make('password123'),
-				'role_id' => config('constants.roles.student'),
-				'section_id' => Section::find(1)->id,
-				'class_number' => 1,
-			]
-		]);
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* Create super user */
+        DB::table('user')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password123'),
+                'role_id' => config('constants.roles.admin'),
+                'section_id' => null,
+                'class_number' => null,
+            ],
+            [
+                'name' => 'Student',
+                'email' => '123456',
+                'password' => Hash::make('password123'),
+                'role_id' => config('constants.roles.student'),
+                'section_id' => Section::find(1)->id,
+                'class_number' => 1,
+            ]
+        ]);
+    }
 }
