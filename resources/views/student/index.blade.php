67826dc566d5d58d258ce6b5492bdd4480a8a168
Justin Parado <justin.parado@dice205.com>
2020-04-05 17:02:58 +0800

    Compelte student list

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -7,42 +7,47 @@
 		<div class="student-list__container">
 			<div class="student-list__actions">
 				<div class="student-list__card">
-					<div class="student-list__card-header">12 ABM 15</div>
-					<div class="student-list__card-body px-0">
-							<div class="student-list__card-body-container">
-								<div class="candidates__position-body">
-									<div class="candidates__position-body-container">
-										<div class="candidates__table-container mx-0 mx-md-4">
-											<table class="candidates__table student-list__table">
-												<thead>
-													<tr>
-														<th>status</th>
-														<th>cn</th>
-														<th>student number</th>
-														<th>full name</th>
-														<th>password</th>
-													</tr>
-												</thead>
-												<tbody>
-													{{-- @foreach($students as $student) --}}
-														<tr class="">
-															<td></td>
-															<td></td>
-															<td></td>
-															<td></td>
-															<td></td>
+
+					@foreach($sections as $section)
+						<div>
+							<div class="student-list__card-header">{{ $section->level }} {{ $section->strand->name }} {{ $section->number }} </div>
+							<div class="student-list__card-body px-0">
+								<div class="student-list__card-body-container">
+									<div class="candidates__position-body">
+										<div class="candidates__position-body-container">
+											<div class="candidates__table-container mx-0 mx-md-4">
+												<table class="candidates__table student-list__table">
+													<thead>
+														<tr>
+															<th>status</th>
+															<th>cn</th>
+															<th>student number</th>
+															<th>full name</th>
+															<th>password</th>
 														</tr>
-														{{-- @endforeach --}}
-												</tbody>
-											</table>
-										</div>
-										<div class="d-flex justify-content-center align-items-center mt-3">
-											<a href="{{route('admin.registryStudent')}}"
-											   class="btn btn-lg client-custom-button-2 no-text-shadow">return</a>
+													</thead>
+													<tbody>
+														@foreach($section->students as $student)
+															<tr class="">
+																<td>{{ count($student->candidates) > 0 ? "Voted" : "No Vote" }}</td>
+																<td>{{ $student->class_number }}</td>
+																<td>{{ $student->email }}</td>
+																<td>{{ $student->name }}</td>
+																<td>Password Here</td>
+															</tr>
+														@endforeach
+													</tbody>
+												</table>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
+						</div>
+					@endforeach
+					<div class="d-flex justify-content-center align-items-center my-3">
+						<a href="{{route('admin.registryStudent')}}"
+						   class="btn btn-lg client-custom-button-2 no-text-shadow">return</a>
 					</div>
 				</div>
 			</div>
