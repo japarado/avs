940d9662214d5f6d549c0ec57b463ab9775a8fb0
Justin Parado <justin.parado@dice205.com>
2020-04-05 15:12:45 +0800

    Student create and store actions

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Section;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
 	public function index() 
 	{
-		return
+	}
+
+	public function create()
+	{
+		$context = [
+			'sections' => Section::with('strand')
+				->orderby('section.level')
+				->orderby('section.strand_id')
+				->orderby('section.number')
+				->get()
+		];
+
+		return view('student.create', $context);
+	}
+
+	public function store(Request $request)
+	{
+		User::updateOrCreate(
+			[
+				'email' => $request->input('student_number'),
+			],
+			[
+				'email' => $request->input('student_number'),
+				'name' => $request->input('name'),
+				'password' => Hash::make($request->input('password')),
+				'class_number' => $request->input('cn'),
+				'section_id' => $request->input('section'),
+				'role_id' => config('constants.roles.student')
+			]
+		);
+
+		return redirect()->back();
 	}
 }
