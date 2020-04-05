2f4071cdf45c0bbb7693554b0c27e14283a429ad
Justin Parado <justin.parado@dice205.com>
2020-04-05 15:47:10 +0800

    section id fillable field

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'student_number', 'class_number'
+        'name', 'email', 'password', 'student_number', 'class_number', 'role_id', 'section_id'
     ];
 
     /**
