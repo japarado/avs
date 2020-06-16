390f7af166c6ee3a41618cc373bebd0e03071b4f
Justin Parado <justin.parado@dice205.com>
2020-06-16 12:29:09 +0800

    Added ckeditor in admin panel

--- resources/views/pollingstation/edit.blade.php
+++ resources/views/pollingstation/edit.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <article class="registry-student">
     <div class="registry-student__container justify-content-center mt-md-7">
+
         <div class="registry-student__actions border-client-yellow registry-student__actions--custom-2 mr-lg-4 flex-basis-50">
             <div class="registry-student__card justify-content-start">
                 <div class="registry-student__card-header bg-client-yellow">Change Password</div>
@@ -40,6 +41,7 @@
                 </form>
             </div>
         </div>
+
         <div class="flex-basis-50">
             <div class="registry-student__actions border-client-yellow registry-student__actions--custom-2 mb-lg-4">
                 <div class="registry-student__card">
@@ -89,9 +91,31 @@
             </div>
         </div>
 </article>
+<article class="registry-student">
+	<div class="registry-student__container justify-content-center">
+		<div class="registry-student__actions border-client-yellow">
+			<div class="registry-student__card">
+				<div class="registry-student__card-header bg-client-yellow">Instructions Page COntents</div>
+				<div class="registry-student__card-body">
+					<div class="registry-student__card-body-container">
+						<div id="js-editor"></div>
+					</div>
+					<div class="d-flex justify-content-center align-items-center no-wrap my-3">
+						<button type="submit"
+								class="btn btn-lg client-custom-button no-text-shadow">submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
 <div class="d-flex justify-content-center align-items-center no-wrap my-3">
     <a href="{{ action('SuperUserController@index') }}" class="btn btn-lg client-custom-button no-text-shadow">back</a>
 </div>
+@endsection
+
+@section('javascript')
+	<script src="{{ asset('js/ckeditor-loader.js') }}"></script>
 @endsection
 
 @section('footer')
