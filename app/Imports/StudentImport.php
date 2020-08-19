ee5ce6ec864ea5ced56f7f7fe0ade99aed36e5a9
Justin Parado <justin.parado.personal@gmail.com>
2020-08-20 00:43:21 +0800

    Fixed column collision issues

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -3,85 +3,71 @@
 namespace App\Imports;
 
 use App\User;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\OnEachRow;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
-use Illuminate\Validation\Rule;
-use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Row;
 
-class StudentImport implements ToCollection, WithHeadingRow, WithValidation, WithBatchInserts
+class StudentImport implements WithHeadingRow, WithValidation, WithBatchInserts, OnEachRow
 {
-    use Importable;
-
-	public function collection(Collection $collection)
+	public function onRow(Row $row)
 	{
+		$row = $row->toArray();
+		$existing_user = User::where('email', $row['student_number'])->first();
+		$altered_user = null;
 
-		$is_valid = Validator::make($collection->toArray(), [
-			'student_number' => 'required',
-			'name' => 'required',
-			'password' => 'required',
-			'class_number' => 'required',
-			'section_id' => 'required',
-		])->validate();
-		foreach($collection as $row)
+		$values = [
+			'name' => $row['name'],
+			'password' => Hash::make($row['password']),
+			'class_number' => $row['class_number'],
+			'section_id' => $row['section_id'],
+			'role_id' => config('constants.roles.student')
+		];
+
+		if($existing_user) {
+			if($existing_user->role_id === config('constants.roles.student')) 
+			{
+				$existing_user->fill($values);
+				$altered_user = $existing_user;
+			}
+		}
+		else
 		{
-
-
-			die($is_valid);
-
-			$return_value = null;
-			$values = [
-				'name' => $row['name'],
-				'password' => Hash::make($row['password']),
-				'class_number' => $row['class_number'],
-				'section_id' => $row['section_id'],
-				'role_id' => config('constants.roles.student')
-			];
-
-			$existing_user = User::firstOrNew(
+			$altered_user = new User();
+			$altered_user->fill(array_merge(
 				[
-					'email' => $row['student_number']
+					'email' => $row['student_number'],
 				],
 				$values
-			);
+			));
+		}
 
-			if($existing_user->role_id !== config('constants.roles.admin'))
-			{
-				$existing_user->fill(array_merge(
-					[
-						'role_id' => config('constants.roles.student'),
-					],
-					$values
-				));
-				$return_value = $existing_user;
-			}
-
-			if($return_value)
-			{
-				$return_value->save();
-			}
+		if($altered_user) 
+		{
+			$altered_user->save();
 		}
 	}
 
+	public function rules(): array
+	{
+		return [
+			'student_number' => 'required',
+			'name' => 'required',
+			'password' => 'required',
+			'section_id' => 'exists:section,id'
+		];
+	}
 
-    public function rules(): array
-    {
-        return [
-            'student_number' => 'required',
-            'name' => 'required',
-            'password' => 'required',
-            'section_id' => 'exists:section,id'
-        ];
-    }
+	public function batchSize(): int
+	{
+		return 500;
+	}
 
-    public function batchSize(): int
-    {
-        return 500;
-    }
+	public function prettyPrint($object) {
+		echo "<pre>";
+		print_r($object);
+		echo "</pre>";
+	}
 }
