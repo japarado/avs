665ee2263e46943cdf0bd490a5c49f3a39bff33e
Justin Parado <justin.parado.personal@gmail.com>
2020-08-20 13:04:02 +0800

    Revert "Removed unused imports"
    This reverts commit 4c94e06b40d733c6d078b1182ed731298c11b77d.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Candidate;
 use App\Imports\StudentImport;
 use App\Section;
 use App\User;
+use Exception; use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Facades\Excel;
 use Maatwebsite\Excel\Validators\ValidationException;
